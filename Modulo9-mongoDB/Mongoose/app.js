ddb4f86f74235fdd469b0d64797bb871c604998e
Renan Pires <renan.sp.121@hotmail.com>
2022-10-25 17:41:57 -0300

    25/10/2022

--- Modulo9-mongoDB/Mongoose/app.js
+++ Modulo9-mongoDB/Mongoose/app.js
@@ -1,21 +1,117 @@
 //jshint esversion:6
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-mongoose.connect( "mongodb://localhost:27017/testeDB", { useNewUrlParser: true } );
+mongoose.connect( "mongodb://127.0.0.1:27017/fruitsDB", { useNewUrlParser: true } );
 
-const testeSchema = new mongoose.Schema({
-    name: String,
-    rating: Number,
+const fruitSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Digite o nome da fruta"]
+    },
+    rating: {
+        type: Number,
+        min: 1,
+        max: 10
+    },
     review: String
 });
 
-const Teste = mongoose.model("Teste", testeSchema);
+const Fruit = mongoose.model("Fruit", fruitSchema);
 
-const teste = new Teste ({
-    name: "Banana",
-    rating: 5,
-    review: "Muito bao"
+const fruit = new Fruit ({
+    name: "Manga",
+    rating: 6,
+    review: "Decent Fruit."
 });
 
-teste.save();
+fruit.save();
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    age: Number,
+    favoriteFruit: fruitSchema
+});
+
+const Person = mongoose.model("Person", personSchema);
+
+Person.updateOne({name: "John"}, {favoriteFruit: fruit}, (err)=>{
+    if (err) {
+        console.log(err);
+    } else {
+        console.log("FOI");
+    }
+});
+
+// Person.deleteMany({name: "John"}, (err) => {
+//     if (err) {
+//         console.log(err);
+//     } else {
+//         console.log("Sucesso");
+//     }
+// });
+
+const person = new Person({
+    name: "John",
+    age: 37,
+    favoriteFruit: fruit
+});
+
+// person.save()
+
+// const kiwi = new Fruit({
+//     name: "kiwi",
+//     rating: 10,
+//     review: "The best."
+// });
+
+// const orange = new Fruit({
+//     name: "orange",
+//     rating: 4,
+//     review: "Too sour."
+// });
+
+// const banana = new Fruit({
+//     name: "banana",
+//     rating: 3,
+//     review: "Weird."
+// });
+
+// Fruit.insertMany([kiwi,orange,banana], (err)=>{
+//     if(err){
+//         console.log(err);
+//     }else {
+//         console.log("Success");
+//     }
+// });
+
+// Fruit.find((err, response)=>{
+//     if (err) {
+//         console.log(err);
+//     } else {
+
+//         mongoose.connection.close();
+
+//         response.forEach(element => {
+//             var fruta = element.name;
+//             console.log(fruta);
+//         });
+//     }
+// });
+
+// Fruit.updateOne({name: "banana"}, {name: "Banana"}, (err) => {
+//     if(err){
+//         console.log(err);
+//     }else {
+//         console.log("Feito");
+//     }
+// });
+
+// Fruit.deleteOne({name: "Banana"}, (err) => {
+//     if(err){
+//         console.log(err);
+//     }else {
+//         mongoose.connection.close();
+//         console.log("Feito");
+//     }
+// });

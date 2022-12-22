a120ff79594c67dda2d6aa7044f266b538e9328e
Renan Pires <renan.sp.121@hotmail.com>
2022-12-22 18:00:43 -0300

    22/12/2022

--- Modulo13(React.JS)/React Local/Keeper/src/components/App.jsx
+++ Modulo13(React.JS)/React Local/Keeper/src/components/App.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-import Footer from "./Footer";
 import Header from "./Header";
+import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
-function App() {
-    return <div>
-        <Header />
-        <Note /> 
-        <Footer />
+export default function App() {
+  return (
+    <div>
+      <Header />
+      {notes.map((noteItem) =>  {
+        return (
+          <Note
+            key={noteItem.key}
+            title={noteItem.title}
+            content={noteItem.content}
+          />
+        );
+      })}
+      <Footer />
     </div>
+  );
 }
-
-export default App;

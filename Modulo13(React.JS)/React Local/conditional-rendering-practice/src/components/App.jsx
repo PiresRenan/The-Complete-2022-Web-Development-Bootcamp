fb9e42b43f5d76d1b71631cf31673a3718fd7271
Renan Pires <renan.sp.121@hotmail.com>
2022-12-26 10:58:56 -0300

    26/12/2022

---
+++ Modulo13(React.JS)/React Local/conditional-rendering-practice/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Form from "./Form";
+
+var userIsRegistered = false;
+
+export default function App() {
+  return (
+    <div className="container">
+      <Form isRegistered={userIsRegistered} />
+    </div>
+  );
+}
+
---
+++ Modulo13(React.JS)/React Local/conditional-rendering-practice/src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Input from "./Input";
+
+export default function Form(props) {
+  return (
+    <form className="form">
+      <Input type="text" placeholder="Username"/>
+      <Input type="password" placeholder="Password"/>
+      {!props.isRegistered && <input type="password" placeholder="Confirm Password" />}
+      <button type="submit">{ props.isRegistered ? "Login" : "Register" }</button>
+    </form>
+  );
+}
